33254460cc90b538c015c835c5eff20fb7cdf0cc
백준규 <jungyu4281@gmail.com>
2024-11-19 11:37:20 +0900

    Work on mongooses

--- data/auth.js
+++ data/auth.js
@@ -1,21 +1,28 @@
-import MongoDb from 'mongodb'
-import { getUsers } from '../db/database.js'
-const ObjectID = MongoDb.ObjectId
+import Mongoose from 'mongoose'
+import { useVirtualId } from '../db/database.js'
 
-export async function findByUsername(username){
-    return getUsers().find({ username }).next().then(mapOptionalUser) //mapOptionalUser = 함수 및 콜백
+const userSchema = new Mongoose.Schema({
+    username: { type: String, require: true },
+    name: { type: String, require: true },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+    url: String
+}, { versionKey: false })
+
+useVirtualId(userSchema)
+const User = Mongoose.model('User', userSchema)
+export async function findByUsername(username) {
+    return User.findOne({ username })
 }
 
 export async function findById(id) {
-    return getUsers().find({ _id: new ObjectID(id)})
-    .next()
-    .then(mapOptionalUser)
+    return User.findById(id)
 }
 
-export async function createUser(user){
-    return getUsers().insertOne(user).then((result) => result.insertedId.toString())
+export async function createUser(user) {
+    return new User(user).save().then((data) => data.id)
 }
 
-function mapOptionalUser(user){ //user를 받아서
-    return user ? { ...user, id: user._id.toString()} : user // user 의 값이 존재한다면 :왼쪽 값 리턴, 그게아니라면 그냥 오른쪽 리턴
+function mapOptionalUser(user) { //user를 받아서
+    return user ? { ...user, id: user._id.toString() } : user // user 의 값이 존재한다면 :왼쪽 값 리턴, 그게아니라면 그냥 오른쪽 리턴
 }
--- db/database.js
+++ db/database.js
@@ -1,18 +1,19 @@
 import { config } from '../config.js'
-import MongoDb from 'mongodb'
-
+import Mongoose from 'mongoose'
 let db
-
 export async function connectDB(){
-    return MongoDb.MongoClient.connect(config.db.host).then((client)=>{
-        db = client.db()
-    })
+    return Mongoose.connect(config.db.host)
 }
-
-export function getUsers() {
+export function useVirtualId(schema){
+    schema.virtual('id').get(function(){
+        return this._id.toString()
+    })
+    schema.set('toJSON', {virtual:true})
+    schema.set('toObject', {virtual:true})
+}
+export function getUsers(){
     return db.collection('users')
 }
-
 export function getTweets(){
     return db.collection('tweets')
 }
